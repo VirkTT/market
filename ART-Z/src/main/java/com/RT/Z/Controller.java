47c704226db262769d1df22b00d29100d3f0f5aa
Fuad Acevedo <fuad.acevedo@gmail.com>
2022-06-28 08:38:34 -0400

    Test de saludos

---
+++ ART-Z/src/main/java/com/RT/Z/Controller.java
@@ -0,0 +1,17 @@
+package com.RT.Z;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class Controller {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Sin Parar de Aprender a Saludar";
+    }
+
+
+}
