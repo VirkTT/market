7ffb836e45c77291589589d99140f7f0adc25341
Fuad Acevedo <fuad.acevedo@gmail.com>
2022-06-28 18:57:24 -0400

    TEST

--- platzi_market-clase-12-conectar-db/src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
+++ platzi_market-clase-12-conectar-db/src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -1,12 +1,9 @@
 package com.platzi.market.persistence.entity;
 
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "compras_producto")
+@Table(name = "compras_productos")
 public class ComprasProducto {
 
     @EmbeddedId
@@ -15,6 +12,14 @@ public class ComprasProducto {
     private Integer cantidad;
     private Double total;
     private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra", insertable = false, updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", insertable = false, updatable = false)
+    private Producto producto;
 
     public ComprasProductoPK getId() {
         return id;
