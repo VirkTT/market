5ef630f8ffd5f68817fe79d877738f6adbdf7ab8
Fuad Acevedo <fuad.acevedo@gmail.com>
2022-06-28 12:48:52 -0400

    TEST

--- ART-Z/src/main/java/com/RT/Z/web/controller/Controller.java
+++ ART-Z/src/main/java/com/RT/Z/web/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.RT.Z;
+package com.RT.Z.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ platzi_market-clase-12-conectar-db/src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -0,0 +1,87 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+
+    private String nombre;
+
+    @Column(name = "id_categoria")
+    private Integer idCroducto;
+
+    @Column(name = "codigo_barras")
+    private String codigBarras;
+
+    @Column(name = "precio_venta")
+    private Double precioVenta;
+
+    @Column(name = "cantidad_stock")
+    private Integer cantidadStock;
+
+    private boolean estado;
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getIdCroducto() {
+        return idCroducto;
+    }
+
+    public void setIdCroducto(Integer idCroducto) {
+        this.idCroducto = idCroducto;
+    }
+
+    public String getCodigBarras() {
+        return codigBarras;
+    }
+
+    public void setCodigBarras(String codigBarras) {
+        this.codigBarras = codigBarras;
+    }
+
+    public Double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(Double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Integer getCantidadStock() {
+        return cantidadStock;
+    }
+
+    public void setCantidadStock(Integer cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
