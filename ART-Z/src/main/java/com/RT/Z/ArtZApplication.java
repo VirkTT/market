0f7c4ce91be01f5f03101f769cb75a7a1fec4e20
VirkTT <fuad.acevedo@gmail.com>
2022-06-27 22:11:00 -0400

    Este es el primer commit de Java

---
+++ ART-Z/src/main/java/com/RT/Z/ArtZApplication.java
@@ -0,0 +1,13 @@
+package com.RT.Z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtZApplication.class, args);
+	}
+
+}
---
+++ ART-Z/src/test/java/com/RT/Z/ArtZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RT.Z;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
